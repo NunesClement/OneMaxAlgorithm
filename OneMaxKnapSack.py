562f762e8535d388dc302a38967a08d54d80fd3e
C_NNS <nunes.clement19@gmail.com>
2021-10-05 16:42:17 +0200

    ++

--- OneMaxKnapSack.py
+++ OneMaxKnapSack.py
@@ -1,10 +1,17 @@
-from random import choices
-from typing import List, Callable
+import time
+from functools import partial
+from random import choices, randint, random, randrange
+from typing import List, Callable, Tuple
 from collections import namedtuple
 
 Genome = List[int]
 Population = List[Genome]
 FitnessFunc = Callable[[Genome], int]
+PopulationFunc = Callable[[], Population]
+SelectionFunc = Callable[[Population, FitnessFunc], Tuple[Genome, Genome]]
+CrossoverFunc = Callable[[Genome, Genome], Tuple[Genome, Genome]]
+MutationFunc = Callable[[Genome], Genome]
+
 Thing = namedtuple('Thing', ['name', 'value', 'weight'])
 
 things = [
@@ -12,8 +19,7 @@ things = [
     Thing('Headphones', 150, 160),
     Thing('Coffee Mug', 60, 350),
     Thing('Notepad', 40, 333),
-    Thing('Water Bottle', 30, 192),
-
+    Thing('Water Bottle', 30, 192)
 ]
 
 more_things = [
@@ -21,12 +27,12 @@ more_things = [
     Thing('Socks', 10, 38),
     Thing('Tissues', 15, 80),
     Thing('Phone', 500, 200),
-    Thing('Baseball Cap', 100, 70),
+    Thing('Baseball Cap', 100, 70)
 ] + things
 
 
 def generate_genome(length: int)  -> Genome:
-    return choices([0, 0], k=length)
+    return choices([0, 1], k=length)
 
 
 def generate_population(size: int, genome_length: int) -> Population:
@@ -59,5 +65,82 @@ def selection_pair(population: Population, fitness_func: FitnessFunc) -> Populat
 # 2 génome en paramètres et 2 génomes rendus
 
 def single_point_crossover(a: Genome, b: Genome) -> Tuple[Genome, Genome]:
+    if(len(a) != len(b)):
+        raise ValueError("Signe_point_crossover tried to compare 2 gen with differents length")
+    length = len(a)
+    if(length < 2):
+        return a, b
     p = randint(1, length -1)
-    return a[0:p] + b[p:]
+    return a[0:p] + b[p:], b[0:p] + a[p:]
+
+
+def mutation(genome: Genome, num: int = 1, probability: float = 0.5) -> Genome :
+    for _ in range(num):
+        index = randrange(len(genome))
+        genome[index] = genome[index] if random() > probability else abs(genome[genome[index] -1])
+        return genome
+
+def run_evolution(
+        populate_func: PopulationFunc,
+        fitness_func: FitnessFunc,
+        fitness_limit: int,
+        selection_func: SelectionFunc = selection_pair,
+        crossover_func: CrossoverFunc = single_point_crossover,
+        mutation_func: MutationFunc = mutation,
+        generation_limit: int = 100
+) -> Tuple[Population, int]:
+    population = populate_func()
+
+    for i in range(generation_limit):
+        population = sorted(
+            population,
+            key= lambda genome: fitness_func(),
+            reverse = True
+        )
+
+        if fitness_func(population[0]) >= fitness_limit:
+            break
+
+        next_generation = population[0:2]
+
+        for j in range(int(len(population) / 2) - 1):
+            parents = selection_func(population, fitness_func)
+            offspring_a, offspring_b = crossover_func(parents[0], parents[1])
+            offspring_a = mutation_func(offspring_a)
+            offspring_b = mutation_func(offspring_b)
+            next_generation += [offspring_a, offspring_b]
+
+        population = next_generation
+    population = sorted(
+        population,
+        key = lambda genome: fitness_func(genome),
+        reverse=True
+    )
+
+    return population, i
+start = time.time()
+
+population, generations = run_evolution(
+    populate_func=partial(
+        generate_population, size=10, genome_length=len(things)
+    ),
+    fitness_func=partial(
+        fitness, things=things, weight_limit=3000
+    ),
+    fitness_limit=740,
+    generation_limit=100
+)
+end = time.time();
+
+def genome_to_things(genome: Genome, things: [Thing]) -> [Thing]:
+    result = []
+    for i, thing in enumerate(things):
+        if genome[i] == 1:
+            result += [thing.name]
+
+    return result
+
+print(f"Number of generations: {generations}")
+print(f"Time: {end- start}s")
+
+print(f"Best solution: {genome_to_things(population[0], things)}")
--- oneMaxGeneralise.py
+++ oneMaxGeneralise.py
@@ -3,6 +3,8 @@
 # TODO Tester les probas
 # TODO Tester la taille des populations
 # TODO Tester les affichages matlabs
+# TODO Faire une interface graphique
+# TODO Conserver les seeds pour refaire les expérimentations sur la même base
 # TODO Rendre généralisable
 
 # Cet algo sera utilisé de façon généralisé de façon à pouvoir l'utiliser avec du QLearning, choix de paramètre avec du renforcement etc..
