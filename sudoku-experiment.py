f351d8133a66678e332e63447eae3ba926018a8f
NunesClement <nunes.clement19@gmail.com>
2024-07-09 15:46:39 +0200

    wip(sudoku)

--- sudoku-experiment.py
+++ sudoku-experiment.py
@@ -1,14 +1,11 @@
 import math
 import time
 import random
+import numpy as np
 
-size = 16
+size = 9
 subSize = int(size / math.sqrt(size))
 binarySize = math.ceil(math.log2(size))
-
-
-import random
-import math
 
 
 def create_sudoku_grid():
@@ -19,8 +16,8 @@ def create_sudoku_grid():
     # Calculate the grid dimensions
     n = int(math.sqrt(size))
 
-    # Initialize the grid with zeros
-    grid = [[random.randint(1, size) for _ in range(size)] for _ in range(size)]
+    # Initialize the grid with random integers from 1 to size (inclusive)
+    grid = np.random.randint(1, size + 1, size=(size, size))
 
     return grid
 
@@ -37,45 +34,43 @@ def display_sudoku_grid(grid):
             if j % subSize == 0 and j != 0:
                 print("|", end=" ")
 
-            print(
-                f"{grid[i][j]:2}", end=" "
-            )  # Print each number with a width of 2 characters
+            print(f"{grid[i][j]:2}", end=" ")
 
         print("|")
 
 
-print(display_sudoku_grid(create_sudoku_grid()))
+# start_time = time.time()
+
+# for i in range(0, 200):
+#     create_sudoku_grid2()
+#     end_time = time.time()
+
+# print(f"Execution time2: {end_time - start_time:.6f} seconds")
 
 
-# print(create_sudoku_grid())
+def digitToBinary(digit):
+    return bin(digit)[2:].zfill(binarySize)
 
 
-# def digitToBinary(digit):
-#     return bin(digit)[2:].zfill(binarySize)
+def convertGridToBinary(grid):
+    binaryGrid = [[digitToBinary(grid[i][j]) for j in range(size)] for i in range(size)]
+    return binaryGrid
 
 
-# def convertGridToBinary(grid):
-#     binaryGrid = [[digitToBinary(grid[i][j]) for j in range(size)] for i in range(size)]
-#     return binaryGrid
+def sub_sudoku_grid():
+    subSize = int(size / math.sqrt(size))
+
+    for i in range(0, size, subSize):
+        for j in range(0, size, subSize):
+            print(str(i) + " " + str(j))
 
 
-# # print(convertGridToBinary(create_sudoku_grid()))
+print(sub_sudoku_grid())
 
-# # print(binaryLengthNeeded(127))
+# start_time = time.time()
 
+# for i in range(0, 100000):
+#     convertGridToBinary(create_sudoku_grid())
+#     end_time = time.time()
 
-# def sub_cube_analyzer():
-#     subSize = int(size / math.sqrt(size))
-
-#     for i in range(0, size, subSize):
-#         for j in range(0, size, subSize):
-#             print(str(i) + str(j))
-
-
-# # start_time = time.time()
-
-# # for i in range(0, 100000):
-# #     sub_cube_analyzer(100)
-# #     end_time = time.time()
-
-# # print(f"Execution time1: {end_time - start_time:.6f} seconds")
+# print(f"Execution time2: {end_time - start_time:.6f} seconds")
