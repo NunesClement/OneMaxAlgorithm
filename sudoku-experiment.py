8fed19ada211a8be18bb10d4397c64424799b3ad
NunesClement <nunes.clement19@gmail.com>
2024-07-09 16:48:30 +0200

    wip(sudoku)

--- sudoku-experiment.py
+++ sudoku-experiment.py
@@ -9,14 +9,11 @@ binarySize = math.ceil(math.log2(size))
 
 
 def create_sudoku_grid():
-    # Check if the size is a perfect square
     if size <= 0 or int(math.sqrt(size)) ** 2 != size:
         raise ValueError("The size must be a perfect square.")
 
-    # Calculate the grid dimensions
     n = int(math.sqrt(size))
 
-    # Initialize the grid with random integers from 1 to size (inclusive)
     grid = np.random.randint(1, size + 1, size=(size, size))
 
     return grid
@@ -39,15 +36,6 @@ def display_sudoku_grid(grid):
         print("|")
 
 
-# start_time = time.time()
-
-# for i in range(0, 200):
-#     create_sudoku_grid2()
-#     end_time = time.time()
-
-# print(f"Execution time2: {end_time - start_time:.6f} seconds")
-
-
 def digitToBinary(digit):
     return bin(digit)[2:].zfill(binarySize)
 
@@ -57,20 +45,61 @@ def convertGridToBinary(grid):
     return binaryGrid
 
 
-def sub_sudoku_grid():
+def get_sub_sudoku_grids(grid):
     subSize = int(size / math.sqrt(size))
+    sub_grids = []
 
     for i in range(0, size, subSize):
         for j in range(0, size, subSize):
-            print(str(i) + " " + str(j))
+            sub_grids.append([i, j])
+
+    return sub_grids
 
 
-print(sub_sudoku_grid())
+def sub_sudoku_grid_penalty(subgrid, grid):
+    subItemX = subgrid[0]
+    subItemY = subgrid[1]
+
+    seen_numbers = set()
+    redundancy_count = 0
+
+    for k in range(subItemX, subItemX + subSize):
+        for l in range(subItemY, subItemY + subSize):
+            num = grid[k][l]
+            if num in seen_numbers:
+                redundancy_count += 1
+            seen_numbers.add(num)
+
+    # print(redundancy_count)
+    return redundancy_count
+
+
+def sudoku_line_penalty(grid):
+    seen_numbers = set()
+    redundancy_count = 0
+
+    for i in range(grid[0], grid[0] + size):
+        num = grid[i]
+        if num in seen_numbers:
+            redundancy_count += 1
+        seen_numbers.add(num)
+
+    return sudoku_line_penalty
+
+
+sudokuGrid = create_sudoku_grid()
+
+display_sudoku_grid(sudokuGrid)
+
+for lines in range(0, size):
+    sudoku_line_penalty(sudokuGrid)
 
 # start_time = time.time()
 
 # for i in range(0, 100000):
-#     convertGridToBinary(create_sudoku_grid())
-#     end_time = time.time()
+#     for subgrid in get_sub_sudoku_grids(sudokuGrid):
+#         sub_sudoku_grid_penalty(subgrid, sudokuGrid)
 
-# print(f"Execution time2: {end_time - start_time:.6f} seconds")
+# end_time = time.time()
+
+# print(f"Execution time: {end_time - start_time:.6f} seconds")
